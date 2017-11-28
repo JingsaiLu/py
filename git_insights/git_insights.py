0be3a49b4994c8cb7bfa83d89a146894e9a11165
kinsey <jingsai.lu@nxp.com>
2017-11-28 19:11:44 +0800

    from process to class

--- git_insights/git_insights.py
+++ git_insights/git_insights.py
@@ -23,6 +23,81 @@ gitcmds = {'weekcommits': 'git log --pretty=oneline --format="%h - %ar - %an : %
             'total_author': 'git log --oneline --pretty="%an"'
 }
 
+
+
+class GitRepoReport(object):
+    """docstring for GitInfo"""
+    def __init__(self, repo_path, repo_branch, output_path):
+        super(GitRepoReport, self).__init__()
+
+        self.git_cmds = {'weekdiff': 'git log -p --since=1.week > ..git.temp',
+            'gitversion': 'git --version',
+            'gitremote': 'git remote -v',
+            'total_commits_win': 'git log --oneline --pretty="%h %an" | find /c " " ',
+            'total_authors': 'git log --oneline --pretty="%an"'
+        }
+
+        self.repo_path = os.path.abspath(repo_path)
+        self.repo_branch = repo_branch
+        self.output_path = os.path.abspath(output_path)
+        self.gen_date = time.asctime(time.localtime(time.time()))
+        self.git_commits = []
+        self.git_version = ''
+        self.git_remote = ''
+
+        # info['total_commits'] = get_pipeout(gitcmds['total_commits_win']).strip('\r\n')
+        # info['total_authors'] = len(set(get_pipeout(gitcmds['total_author']).strip('\n').split('\n')))
+        
+    def get_pipeout(self, cmd, cwd=None):
+        if cwd is None:
+            cwd = self.repo_path
+        p = subprocess.Popen(cmd, 0, None, None, subprocess.PIPE, subprocess.PIPE, cwd=cwd, shell=True)
+        str =''
+        for line in p.stdout:
+            str = str + line
+        p.wait()
+        return str.strip('\n')
+
+    def get_weeeks_commits(self, weeks='1'):
+        cmd = 'git log --pretty=oneline --format="%h - %ar - %an : %s" --since='+str(weeks)+'.week'
+        git_commits = []
+        commits_list = self.get_pipeout(cmd).split('\n')
+        i = 0
+        for k in commits_list:
+            commit_list = k.split(' - ')
+            git_commits.append([])
+            git_commits[i].append(commit_list[0])
+            git_commits[i].append(commit_list[1])
+            s_split = ''.join(commit_list[2:]).split(' : ')
+            git_commits[i].append(s_split[0])
+            git_commits[i].append(s_split[1])
+            i = i + 1
+        return git_commits
+
+    def get_git_version(self):
+        return self.get_pipeout(self.git_cmds['gitversion'])
+
+    def get_git_remote(self):
+        return self.get_pipeout(self.git_cmds['gitremote'])
+
+    def get_git_allcommits(self):
+        return self.get_pipeout(self.git_cmds['total_commits_win'])
+
+    def get_git_authors(self):
+        return len(set(self.get_pipeout(gitcmds['total_author']).split('\n')))
+
+    def get_diff_stat(self, commit_1, commit_2):
+        cmd = ' '.join(['git diff',commit_1,commit_2,'--stat'])
+        return self.get_pipeout(cmd)
+
+    def generate(self, weeks):
+        self.git_commits = self.get_weeeks_commits(weeks)
+        self.git_version = self.get_git_version()
+        self.git_remote = self.get_git_remote()
+        self.total_commits = self.get_git_allcommits()
+        self.total_authors = self.get_git_authors()
+        self.weekly_diff = self.get_diff_stat(self.git_commits[0][0], self.git_commits[1][0])
+
 def get_pipeout(cmd):
     p = subprocess.Popen(cmd, 0, None, None, subprocess.PIPE, subprocess.PIPE,shell=True)
     str =''
@@ -77,7 +152,7 @@ def get_gitdiff_patch():
             patch = patch + line
             if line.encode('utf-8').startswith('commit '):
                 i = i + 1
-        print 'commit number:', i
+        # print 'commit number:', i
     os.remove('..git.temp')
     return cgi.escape(patch)
 
@@ -142,11 +217,12 @@ def gen_general_info(repo_path, repo_branch, report_path):
     os.chdir(cwd)
     return info
 def get_commit_file(repo_path, commit):
+    commit_file = {}
     cwd = os.getcwd()
     os.chdir(repo_path)
     cmd = 'git show ' + commit + ' --stat'
-    print cmd
-    print get_pipeout(cmd)
+    # print cmd
+    print get_pipeout(cmd).split('\n\n')[2].strip('\n').split('\n')[0:-1]
     os.chdir(cwd)
 def main():
 
@@ -157,7 +233,16 @@ def main():
     gen_report(args['out'], 'summary', general_info, git_log, git_diff_detail)
 
     get_commit_file(args['d'], git_log[0][0])
-    webbrowser.open('//'.join([os.getcwd(),args['out'],'general.html']))
+    # webbrowser.open('//'.join([os.getcwd(),args['out'],'general.html']))
+
+    # args['d'] = 'C:\git\mcu_PSDK_test_imx8qxp_m4\mcu-sdk'
+    report = GitRepoReport(args['d'], 'master', args['out'])
+    print report.get_pipeout(gitcmds['gitversion'])
+    report.generate(1)
+    print report.total_authors
+    print report.git_version
+    print report.weekly_diff
+
  
 if __name__ == '__main__':
     main()
