88e9ced5edffde7ff28dc808e37103f0c62e944e
kinsey <jingsai.lu@nxp.com>
2017-11-29 19:05:51 +0800

    add achors for each diff files

--- git_insights/git_insights.py
+++ git_insights/git_insights.py
@@ -20,7 +20,7 @@ class GitRepoCollector(object):
     def __init__(self, repo_path, repo_branch, output_path):
         super(GitRepoCollector, self).__init__()
 
-        self.git_cmds = {'weekdiff': 'git log -p --since=1.week > ..git.temp',
+        self.git_cmds = {'weekdiff': 'git log -p --since=5.week',
             'gitversion': 'git --version',
             'gitremote': 'git remote -v',
             'total_commits_win': 'git log --oneline --pretty="%h %an" | find /c " " ',
@@ -48,6 +48,8 @@ class GitRepoCollector(object):
         git_commits = []
         commits_list = self.get_pipeout(cmd).split('\n')
         i = 0
+        if commits_list == ['']:
+            return ''
         for k in commits_list:
             commit_list = k.split(' - ')
             git_commits.append([])
@@ -63,7 +65,7 @@ class GitRepoCollector(object):
         return self.get_pipeout(self.git_cmds['gitversion'])
 
     def get_git_remote(self):
-        return self.get_pipeout(self.git_cmds['gitremote'])
+        return self.get_pipeout(self.git_cmds['gitremote']).split('\n')[0].split('\t')[1].split(' ')[0]
 
     def get_git_allcommits(self):
         return self.get_pipeout(self.git_cmds['total_commits_win'])
@@ -87,7 +89,11 @@ class GitRepoCollector(object):
         self.git_remote = self.get_git_remote()
         self.total_commits = self.get_git_allcommits()
         self.total_authors = self.get_git_authors()
-        self.weekly_diff = self.get_diff_stat(self.git_commits[0][0], self.git_commits[1][0])
+        print self.git_commits
+        if self.git_commits=='':
+            self.weekly_diff ='No commit in recent weeks.'
+        else:
+            self.weekly_diff = self.get_diff_stat(self.git_commits[0][0], self.git_commits[len(self.git_commits)-1][0])
         self.commits_stat = self.get_commits_stat()
 
 def get_gitdiff_patch(): 
@@ -102,26 +108,6 @@ def get_gitdiff_patch():
         # print 'commit number:', i
     os.remove('..git.temp')
     return cgi.escape(patch)
-
-def gen_general_report(out_dir, tpl_name, general_info):
-    template = env.get_template(tpl_name+'.tpl')
-    r_content = template.render(general_info = general_info)
-    
-    with codecs.open('//'.join([out_dir, tpl_name+'.html']), 'w', 'utf-8') as fd:
-        fd.write(r_content)   
-
-def gen_detail_report(out_dir, tpl_name, git_diff_patch):
-    template = env.get_template(tpl_name+'.tpl')
-    r_content = template.render(git_diff_patch=git_diff_patch)
-    
-    with codecs.open('//'.join([out_dir, tpl_name+'.html']), 'w', 'utf-8') as fd:
-        fd.write(r_content)   
-
-def gen_report(out_dir, tpl_name, general_info, git_list,git_diff_stat):
-    gen_report_files(out_dir)
-    gen_general_report(out_dir, 'general', general_info)
-    gen_summary_report(out_dir, tpl_name, git_list, git_diff_stat)
-    gen_detail_report(out_dir, 'detail', get_gitdiff_patch())
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Git Weekly Report manual')
@@ -141,35 +127,77 @@ class ReportGenerator(object):
         
     def init_output(self):
         if os.path.exists(self.output_path):
-            shutil.rmtree(self.output_path)
+            os.rename(self.output_path, 'output_del')
+            shutil.rmtree('output_del')
         os.mkdir(self.output_path)
         shutil.copytree('./tpl/css', '//'.join([self.output_path,'css']))
         shutil.copytree('./tpl/js', '//'.join([self.output_path,'js']))
         shutil.copytree('./tpl/img', '//'.join([self.output_path,'img']))
 
+    def gen_general_report(self, tpl_name):
+        template = env.get_template(tpl_name+'.tpl')
+        r_content = template.render(general_info = self.repo)
+        with codecs.open('//'.join([self.output_path, tpl_name+'.html']), 'w', 'utf-8') as fd:
+            fd.write(r_content)        
+
     def gen_summary_report(self, tpl_name):
         commits_stat = {}
         for i, v in self.repo.commits_stat.items():
-            commits_stat[i] = cgi.escape('<a>'+ '</a><br><a>'.join(v) + '</a>')
+            k = 0; str_f = ''
+            for j in v:
+                str_f = str_f + "'<a href='./detail.html#" + str(i) + str(k) + "'>" + j + "</a><br>"
+                k = k + 1
+            commits_stat[i] = str_f
             print commits_stat[i]
-
-
+            # commits_stat[i] = cgi.escape('<a>'+ '</a><br><a>'.join(v) + '</a>')
         template = env.get_template(tpl_name+'.tpl')
         r_content = template.render(git_list = self.repo.git_commits, git_weekly_diff=self.repo.weekly_diff,commits_stat = commits_stat)
         with codecs.open('//'.join([self.output_path, tpl_name+'.html']), 'w', 'utf-8') as fd:
             fd.write(r_content)
 
+    def gen_detail_report(self, tpl_name):
+        new_patch = ''
+        if self.repo.git_commits=='':
+            new_patch = 'No commit in recent weeks.'
+        else:
+            patch = self.repo.get_pipeout(self.repo.git_cmds['weekdiff'])
+            patch = cgi.escape(patch).decode('utf-8')
+            i = -1; j = 0
+            for line in patch.split('\n'):
+                if line.startswith('commit '):
+                    i = i + 1
+                    j = 0
+                if line.startswith('diff --git'):
+                    mao = str(self.repo.git_commits[i][0]) + str(j)
+                    line = "<code name='" + mao + "' id='" + mao +"'></code>" + line + "------------------" + mao + "\n"
+                    j = j + 1
+                if line.startswith('+'):
+                    line = '<code style="color: red">' + line + "</code>"
+                if line.startswith('-'):
+                    line = '<code style="color: green">' + line + "</code>"
+                new_patch = new_patch + line + "\n"
+
+        # patch = '<code style="color: red">' + patch + "</code>"
+        template = env.get_template(tpl_name+'.tpl')
+        r_content = template.render(git_diff_patch=new_patch)
+        
+        with codecs.open('//'.join([self.output_path, tpl_name+'.html']), 'w', 'utf-8') as fd:
+            fd.write(r_content)   
+
     def generate_report(self):
+        self.gen_general_report('general')
         self.gen_summary_report('summary')
+        self.gen_detail_report('detail')
+
 def main():
 
     args = parse_args()
     # webbrowser.open('//'.join([os.getcwd(),args['out'],'general.html']))
 
-    # args['d'] = 'C:\git\mcu_PSDK_test_imx8qxp_m4\mcu-sdk'
+    args['d'] = 'C:\git\mcu_PSDK_test_imx8qxp_m4\mcu-sdk'
     # args['d'] = '../'
     repo = GitRepoCollector(args['d'], 'master', args['out'])
-    repo.generate(100)
+    repo.generate(1)
     report = ReportGenerator(repo, args['out'])
     report.generate_report()
 
--- git_insights/tpl/general.tpl
+++ git_insights/tpl/general.tpl
@@ -9,14 +9,14 @@
     <div id="general">
         <div>
             <dl>
-                <dt>Tool version:</dt><dd>{{general_info['git_version']}} </dd><br>
-                <dt>Date of report:</dt><dd>{{general_info['gen_time']}} </dd><br>
-                <dt>Git repo:</dt><dd>{{general_info['git_remote']}} </dd><br>
-                <dt>Git repo path:</dt><dd>{{general_info['repo_path']}} </dd><br>
-                <dt>Git repo branch:</dt><dd>{{general_info['repo_branch']}} </dd><br>
-                <dt>Report path:</dt><dd>{{general_info['report_path']}} </dd><br>
-                <dt>Total commits:</dt><dd>{{general_info['total_commits']}} </dd><br>
-                <dt>Total author:</dt><dd>{{general_info['total_authors']}} </dd><br>
+                <dt>Tool version:</dt><dd>{{general_info.git_version}} </dd><br>
+                <dt>Date of report:</dt><dd>{{general_info.gen_date}} </dd><br>
+                <dt>Git repo:</dt><dd>{{general_info.git_remote}} </dd><br>
+                <dt>Git repo path:</dt><dd>{{general_info.repo_path}} </dd><br>
+                <dt>Git repo branch:</dt><dd>{{general_info.repo_branch}} </dd><br>
+                <dt>Report path:</dt><dd>{{general_info.output_path}} </dd><br>
+                <dt>Total commits:</dt><dd>{{general_info.total_commits}} </dd><br>
+                <dt>Total author:</dt><dd>{{general_info.total_authors}} </dd><br>
             </dl>
             
                        
